6d7982e1ed2bde50d3216ac2478509c4dabf9591
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-14 09:43:06 -0300

    Final - Passo 5

---
+++ exercicioCinco.js
@@ -0,0 +1,13 @@
+const clinica = [];
+clinica.push("Cobra");
+clinica.push("Cachorro");
+clinica.push("tigre");
+
+console.log(clinica);
+
+clinica.pop();
+clinica.pop();
+clinica.pop();
+
+console.log(clinica);
+
